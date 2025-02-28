153b300b5177d2dfb1f66a13312b19f34ac3bace
Robert <robert10290@gmail.com>
2025-02-28 03:50:32 +0100

    Initial commit

---
+++ src/gui/LoginGui.java
@@ -0,0 +1,13 @@
+package gui;
+
+public class LoginGui extends BaseFrame{
+
+    public LoginGui(){
+        super("Bank App");
+    }
+
+    @Override
+    protected void addGuiComponents() {
+
+    }
+}
