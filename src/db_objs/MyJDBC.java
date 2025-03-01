7dd24dbed19a3f1eaed6108789dad2b9e4a17806
Robert <robert10290@gmail.com>
2025-03-01 16:08:27 +0100

    added RegisterGui functionality

--- src/db_objs/MyJDBC.java
+++ src/db_objs/MyJDBC.java
@@ -26,4 +26,35 @@ public class MyJDBC {
         }
         return null;
     }
+
+    public static boolean register(String username, String password) {
+        try {
+            if (!checkUser(username)) {
+                Connection connection = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD);
+                PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO users (username, password) VALUES(?, ?)");
+                preparedStatement.setString(1, username);
+                preparedStatement.setString(2, password);
+                preparedStatement.executeUpdate();
+                return true;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    private static boolean checkUser(String username) {
+        try {
+            Connection connection = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD);
+            PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM users WHERE username = ?");
+            preparedStatement.setString(1, username);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            if (!resultSet.next()) {
+                return false;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return true;
+    }
 }
--- src/gui/RegisterGui.java
+++ src/gui/RegisterGui.java
@@ -1,7 +1,14 @@
 package gui;
 
+import com.mysql.cj.log.Log;
+import db_objs.MyJDBC;
+
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class RegisterGui extends BaseFrame {
 
@@ -50,12 +57,50 @@ public class RegisterGui extends BaseFrame {
         JButton registerButton = new JButton("Register");
         registerButton.setBounds(20, 460, getWidth() - 50, 40);
         registerButton.setFont(new Font("Dialog", Font.BOLD, 20));
+        registerButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String username = usernameField.getText();
+                String password = String.valueOf(passwordField.getPassword());
+                String rePassword = String.valueOf(rePasswordField.getPassword());
+
+                if (validateUserInput(username, password, rePassword)){
+                    if(MyJDBC.register(username, password)){
+                        RegisterGui.this.dispose();
+                        LoginGui loginGui = new LoginGui();
+                        loginGui.setVisible(true);
+
+                        JOptionPane.showMessageDialog(loginGui, "Registered Account Successfully!");
+                    } else {
+                        JOptionPane.showMessageDialog(RegisterGui.this, "Error: Username already taken");
+                    }
+                } else {
+                    JOptionPane.showMessageDialog(RegisterGui.this, "Error: Username must be at least 6 characters\n"
+                            + "and/or Password must match");
+                }
+
+            }
+        });
         add(registerButton);
 
         JLabel loginLabel = new JLabel("<html><a href=\"#\">Have an account? Sign-in here</a></html>");
         loginLabel.setBounds(0, 510, getWidth() - 10, 30);
         loginLabel.setFont(new Font("Dialog", Font.PLAIN, 20));
         loginLabel.setHorizontalAlignment(SwingConstants.CENTER);
+        loginLabel.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                RegisterGui.this.dispose();
+                new LoginGui().setVisible(true);
+            }
+        });
         add(loginLabel);
+    }
+
+    private boolean validateUserInput(String username, String password, String rePassword) {
+        if (username.length() == 0 || password.length() == 0 || rePassword.length() == 0) return false;
+        if (username.length() < 6) return false;
+        if (!password.equals(rePassword)) return false;
+        return true;
     }
 }
